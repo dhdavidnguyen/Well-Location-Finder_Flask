d0fa633835e2567082df36e079a44b1948cbd5bb
dhdavidnguyen@gmail.com <32910405+davidnguyn@users.noreply.github.com>
2020-02-19 11:56:57 -0800

    initial commit

---
+++ wellfinder.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import logging
+from flask import Flask, request, abort, render_template
+from urllib.parse import urljoin
+
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', None)
+
+
+# well = '41-31R'
+
+def flaskentrypoint(path, wellname):
+    """
+
+    :param path: path of csv file
+    :param well: well being searched
+    :return: google maps link of well
+    """
+    df = pd.read_csv(path)
+    # logging.debug('original df %s' % df.head(1))
+
+    lat = df.loc[df['WellNumber'].str.lower() == wellname.lower(), 'Latitude'].to_string(index=False)
+    long = df.loc[df['WellNumber'].str.lower() == wellname.lower(), 'Longitude'].to_string(index=False)
+    latlong = '{0},{1}'.format(lat, long).lstrip()
+    logging.debug('lat:%s' % latlong)
+
+    map_link = urljoin('https://www.google.com/maps/dir/', '?api=1&destination={0}'.format(latlong))
+    # map_link = df.loc[df['WellNumber'].str.lower() == wellname.lower(), 'Google Maps Link']
+    logging.debug('maplink %s' % map_link)
+
+    # NEED TO ADD EXCEPTION FOR WELL doesn't exist
+
+    return map_link
+
+""" FLASK PORTION OF CODE"""
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    """Proof of life for testing. Is the server up?"""
+    return "Hello Flask Well Finder!"
+
+@app.route('/wellfinder/', methods=['GET', 'POST'])
+def wellfinder():
+    if request.method == 'GET':
+        """First, serving input form as a webpage"""
+        return render_template('wellfinder.html')
+    else:
+        """Otherwise this is a POST to call the imported function"""
+        wellname = request.form['wellname']
+        logging.debug('inputs: {}'.format(wellname))
+        try:
+            path = 'AllWells.csv'
+            maplink = flaskentrypoint(path, wellname)
+            return render_template('results.html', maplink=maplink)
+        except Exception as e:
+            traceback.print_exc()
+            abort(500, str(e))
+
+if __name__ == '__main__':
+    app.run()
+    # path = 'AllWells.csv'
+    # wellname = '41-31R'
+    # maplink = flaskentrypoint(path, wellname)
+    # print(maplink.to_string(index=False))
